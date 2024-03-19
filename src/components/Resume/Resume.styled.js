195635ab7ed40c64aace49f5b375d2e2b5c77175
Maciej Necka <macieknecka@gmail.com>
2024-03-19 17:08:47 +0100

    Button fixes

--- src/components/Resume/Resume.styled.js
+++ src/components/Resume/Resume.styled.js
@@ -29,8 +29,8 @@ const StyledResume = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    font-size: 1.8rem;
-    letter-spacing: 1px;
+    font-size: 2rem;
+    letter-spacing: 1.2px;
     font-weight: bold;
     padding: 1.75rem 7rem;
     background-color: var(--main-color-blue);
